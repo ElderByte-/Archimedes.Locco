86adf9e5f62f35391e60d36261fa37d3dbc13733
IsNull <pascalbuettiker@bluewin.ch>
2015-10-09 13:22:55 +0200

    support default proxy

--- Archimedes.Locco/BackendProviders/GitHubProvider.cs
+++ Archimedes.Locco/BackendProviders/GitHubProvider.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Octokit;
+using Octokit.Internal;
 
 namespace Archimedes.Locco.BackendProviders
 {
     public class GitHubProvider : IReportBackend
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private readonly IPropertyProvider _propertyProvider;
 
         public GitHubProvider(IPropertyProvider propertyProvider)
@@ -30,18 +35,29 @@ namespace Archimedes.Locco.BackendProviders
             var repoOwner = _propertyProvider.GetProperty("locco.github.owner");
             var repoName = _propertyProvider.GetProperty("locco.github.repository");
 
+            // Support default proxy
+            var proxy = WebRequest.DefaultWebProxy;
+            var httpClient = new HttpClientAdapter(() => HttpMessageHandlerFactory.CreateDefault(proxy));
+            var connection = new Connection(new ProductHeaderValue(appId), httpClient);
 
-            var client = new GitHubClient(new ProductHeaderValue(appId))
+            if (proxy != null)
+            {
+                Log.Info(string.Format("Using proxy server '{0}' to connect to github!", proxy.GetProxy(new Uri("https://api.github.com/"))));
+            }
+
+
+            var client = new GitHubClient(connection)
             {
                 Credentials = new Credentials(accessToken)
             };
+
 
             // Creates a new GitHub Issue
             var newIssue = new NewIssue(report.Title)
             {
                 Body = MarkdownReportUtil.CompileBodyMarkdown(report, 220000),
             };
-            //newIssue.Labels.Add("locco");
+            newIssue.Labels.Add("bot");
 
             var issue = await client.Issue.Create(repoOwner, repoName, newIssue);
         }
